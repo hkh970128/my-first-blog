739719d228baf9acbdeef31b5c5ae1988a0b5078
hkh970128 <hkh970128@naver.com>
2021-08-17 21:33:12 +0900

    dd

--- web/settings.py
+++ web/settings.py
@@ -122,9 +122,10 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, '/static/')
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR,'static'),
+    os.path.join(BASE_DIR,'han/static'),
 ]
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+ 
