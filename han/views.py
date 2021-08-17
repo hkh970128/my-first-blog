0f44dc9e9e7673eeac1210611320a855502b651e
hkh970128 <hkh970128@naver.com>
2021-08-17 18:05:43 +0900

    Changed the HTML for the site.

--- han/views.py
+++ han/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
+from django.urls.conf import include
+from .models import Post
+from django.utils import timezone
 
 # Create your views here.
-def home(request):
-    return render(request, 'han.html')
+def post_list(request):
+    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    return render(request, 'post_list.html', {'posts': posts})
